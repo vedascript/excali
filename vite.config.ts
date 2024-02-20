ce619f66e4ce12e4fa8a74e0cb63578b5cce20e3
Vedansh Wani <me.vedanshwani@gmail.com>
2024-02-20 21:17:00 +0530

    inital commit

---
+++ vite.config.ts
@@ -0,0 +1,12 @@
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import checker from "vite-plugin-checker";
+
+export default defineConfig({
+  plugins: [
+    react(),
+    checker({
+      typescript: true,
+    }),
+  ],
+});
